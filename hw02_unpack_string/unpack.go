3b894d17d36deec14da7f10e692fdc4f87653d7a
Lev Nemirovskii <levnemir@yandex.ru>
2023-09-10 00:15:24 +0300

    Полностью готовый функционал, включая дополнительные тесты

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -11,30 +11,57 @@ import (
 // ErrInvalidString is returned when the input string is invalid.
 var ErrInvalidString = errors.New("invalid string")
 
-// Unpack returns the unpacked string or an error if the input string is invalid.
+// Unpack возвращает распакованную строку.
 func Unpack(s string) (string, error) {
-	// создаем билдер для формирования результирующей строки
 	var result strings.Builder
 	var prev rune
-	var prevIsDigit bool
+	var prevIsDigit, prevEscaped bool
 
 	// проходим по всем символам входной строки
 	for idx := 0; idx < len(s); {
 		curR, size := utf8.DecodeRuneInString(s[idx:])    // получаем текущий символ и его размер
-		nextR, _ := utf8.DecodeRuneInString(s[idx+size:]) // получаем следующий символ и его размер
+		nextR, _ := utf8.DecodeRuneInString(s[idx+size:]) // получаем следующий символ
+
+		if curR == '\\' { // текущий символ-экранирование
+			if prevEscaped {
+				prev = curR
+				prevIsDigit = false
+				prevEscaped = false
+				result.WriteString(string(curR))
+			} else {
+				prevEscaped = true
+			}
+
+			idx += size
+			continue
+		}
+
+		if prevEscaped { // если предыдущий символ - экранирование
+			result.WriteString(string(curR))
+			prevEscaped = false
+			prev = curR
+			idx += size // move to next
+			continue
+		}
 
 		// если текущий символ - буква
 		if unicode.IsLetter(curR) {
-			// если следующий символ - 0, то ничего не делаем
-			if nextR == '0' {
-				idx += size
-				prev = curR
-				prevIsDigit = unicode.IsDigit(curR)
-				continue
+			if prevEscaped {
+				repeatCount, _ := strconv.Atoi(string(curR))
+				result.WriteString(strings.Repeat(string(prev), repeatCount-1))
+			} else {
+				// если следующий символ - 0, то ничего не делаем
+				if nextR == '0' {
+					idx += size
+					prev = curR
+					prevIsDigit = unicode.IsDigit(curR)
+					continue
+				}
+
+				result.WriteString(string(curR))
+				prevIsDigit = false
 			}
 
-			result.WriteString(string(curR))
-			prevIsDigit = false
 			prev = curR
 		}
 
@@ -44,6 +71,15 @@ func Unpack(s string) (string, error) {
 			if curR == '0' && !prevIsDigit {
 				prev = curR
 				prevIsDigit = unicode.IsDigit(curR)
+				idx += size
+				continue
+			}
+
+			// если предыдущий символ - экранирование, то записываем цифру
+			if prevEscaped {
+				prevEscaped = false
+				prev = curR
+				result.WriteString(string(curR))
 				idx += size
 				continue
 			}
